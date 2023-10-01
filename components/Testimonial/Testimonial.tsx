6ec56778a4600e053ea4be8091d34c21af32251a
AkashSasikumar47 <akashsasikumar47@gmail.com>
2023-10-02 01:42:59 +0400

    Update Testimonial.tsx

--- components/Testimonial/Testimonial.tsx
+++ components/Testimonial/Testimonial.tsx
@@ -8,13 +8,13 @@ const Testimonial = () => {
                     <h2 className="text-3xl md:text-4xl font-semibold text-black">Participant Testimonials</h2>
                 </div>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-                    <div className="bg-gray-100 p-6 rounded-lg shadow-lg">
+                    <div className="bg-gray-100 p-6 rounded-3xl shadow-lg">
                         <div className="text-lg md:text-xl font-semibold text-black mb-4">
                             "CONCEPTO23 was a game-changer for me. The insights I gained from the industry speakers and the hands-on experience in the competitions were invaluable. It's not just an event; it's a journey of growth and inspiration."
                         </div>
                         <div className="text-base md:text-lg font-normal text-gray-500">- Sarah D, Engineering Student</div>
                     </div>
-                    <div className="bg-gray-100 p-6 rounded-lg shadow-lg">
+                    <div className="bg-gray-100 p-6 rounded-3xl shadow-lg">
                         <div className="text-lg md:text-xl font-semibold text-black mb-4">
                             "I've attended CONCEPTO23 for the past three years, and each time, it gets better. The networking opportunities, the innovative ideas, and the sense of community make it a must-attend event for anyone passionate about technology and innovation."
                         </div>
