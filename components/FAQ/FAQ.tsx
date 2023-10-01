55b6271e183a4e9a8bc35cba9c7fa3f71f756cf0
AkashSasikumar47 <akashsasikumar47@gmail.com>
2023-10-02 01:43:01 +0400

    Update FAQ.tsx

--- components/FAQ/FAQ.tsx
+++ components/FAQ/FAQ.tsx
@@ -8,13 +8,13 @@ const FAQ = () => {
                     <h2 className="text-3xl md:text-4xl font-semibold text-black mb-6">Frequently Asked Questions</h2>
                 </div>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-                    <div className="bg-gray-900 text-white p-6 rounded-lg shadow-lg">
+                    <div className="bg-gray-900 text-white p-6 rounded-3xl shadow-lg">
                         <h3 className="text-xl md:text-2xl font-semibold mb-4">How can I register for CONCEPTO23?</h3>
                         <p className="text-base md:text-lg">
                             Registering for CONCEPTO23 is easy! Simply click on the "Register Now" button on our website, fill out the registration form, and you're all set to join us for the event.
                         </p>
                     </div>
-                    <div className="bg-gray-900 text-white p-6 rounded-lg shadow-lg">
+                    <div className="bg-gray-900 text-white p-6 rounded-3xl shadow-lg">
                         <h3 className="text-xl md:text-2xl font-semibold mb-4">Is there a registration fee for CONCEPTO23?</h3>
                         <p className="text-base md:text-lg">
                             No, registration for CONCEPTO23 is completely free. We believe in making this valuable event accessible to all interested participants.
