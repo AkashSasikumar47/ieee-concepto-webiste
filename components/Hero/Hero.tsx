0a4e1e23472fea81a4e2b94a95ac22766b9133b4
AkashSasikumar47 <akashsasikumar47@gmail.com>
2023-10-02 02:40:38 +0400

    Update Hero.tsx

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,42 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Hero = () => {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        setIsMobile(window.innerWidth <= 768);
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 768);
+        };
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
-        <section className="bg-white text-black mb-16">
-            <div className="w-full md:w-[1920px] h-[883px] px-4 md:px-[84px] py-3 flex flex-col md:flex-row justify-start items-center gap-2.5">
-                <div className="w-full md:w-[1680px] h-[859px] relative">
-                    <div className="w-[174px] h-[435px] left-[1311px] top-[322px] absolute bg-gray-500 bg-opacity-80 rounded-full blur-[134px]" />
-                    <img className="w-full md:w-[1680px] h-[533px] left-0 top-0 absolute rounded-[40px]" src="https://via.placeholder.com/1680x533" alt="Image" />
-                    <div className="w-full md:w-[701px] left-0 top-[573px] absolute text-black text-lg font-medium font-['Plus Jakarta Sans'] leading-[33px]">
-                        Welcome to CONCEPTO23 - Unleash Innovation!<br />Join us in our annual celebration of innovation, technology, and ideation. Discover a world of opportunities, learn from industry experts, and unlock your inner techie. Get ready for a journey that will spark your creativity and drive your passion for innovation. Let's innovate together!
-                    </div>
-                    <div className="left-0 top-[770px] absolute text-black text-2xl font-semibold font-['Plus Jakarta Sans'] underline">Learn More</div>
-                    <div className="w-full md:w-[704px] left-[64px] top-[198px] absolute text-black text-8xl font-extrabold font-['Plus Jakarta Sans']">CONCEPTO23</div>
-                    <img className="w-full md:w-[767px] h-[767px] left-[1625px] top-[1px] absolute origin-top-left rotate-180" src="https://via.placeholder.com/767x767" alt="Image" />
-                    <div className="w-full md:w-[266px] h-[145px] left-[1338px] top-[325px] absolute">
-                        <div className="w-full h-full left-0 top-0 absolute bg-white rounded-3xl shadow" />
-                        <div className="left-[70px] top-[28px] absolute text-black text-lg font-semibold font-['Plus Jakarta Sans']">Exercising</div>
-                        <div className="w-[193.17px] h-[44.47px] left-[36px] top-[80px] absolute" />
-                    </div>
-                    <div className="w-full md:w-[464px] h-[130px] left-[1024px] top-[729px] absolute">
-                        <div className="w-full h-full left-0 top-0 absolute bg-black rounded-3xl shadow" />
-                        <div className="left-[70px] top-[80px] absolute text-white text-2xl font-extrabold font-['Plus Jakarta Sans'] tracking-[12px]">352D 5H 49M 20S</div>
-                        <div className="left-[70px] top-[29px] absolute text-white text-lg font-semibold font-['Plus Jakarta Sans']">Total time in space </div>
-                        <div className="w-10 h-10 left-[20px] top-[20px] absolute bg-slate-400 rounded-xl" />
-                    </div>
-                    <div className="w-full md:w-[337px] h-[199px] left-[799px] top-[410px] absolute flex flex-col md:flex-row justify-start items-start">
-                        <div className="w-full md:w-[337px] h-[199px] relative">
-                            <div className="w-full h-full left-0 top-0 absolute bg-white rounded-3xl shadow" />
-                            <div className="w-10 h-10 left-[20px] top-[20px] absolute bg-slate-400 rounded-xl" />
-                            <div className="left-[70px] top-[29px] absolute text-black text-lg font-semibold font-['Plus Jakarta Sans']">Spacewalks</div>
-                            <div className="w-[190.83px] h-[98.47px] left-[20px] top-[80px] absolute" />
-                            <div className="w-[94.86px] h-[94.71px] left-[222px] top-[83px] absolute" />
+        <div className="relative bg-white mx-auto max-w-[1440px] px-4 md:px-6 lg:px-8 mb-16">
+            {/* Desktop Section */}
+            {!isMobile && (
+                <div className="hidden md:block relative rounded-xl overflow-hidden">
+                    <video className="object-cover object-center w-full h-[400px] rounded-3xl" autoPlay loop muted controls={false}>
+                        <source src="Assets/Videos/Hero_video.mp4" type="video/mp4" />
+                        Your browser does not support the video tag.
+                    </video>
+                    <div className="absolute inset-0 flex flex-col items-left justify-center">
+                        <div className="text-white p-8 text-left w-auto">
+                            <div className="text-8xl font-extrabold font-['Plus Jakarta Sans']">
+                                CONCEPTO-23
+                            </div>
                         </div>
-                        <img className="w-[19.83px] h-[19.83px]" src="https://via.placeholder.com/20x20" alt="Icon" />
                     </div>
                 </div>
-            </div>
-        </section>
+            )}
+
+            {/* Mobile Section */}
+            {isMobile && (
+                <div className="md:hidden relative">
+                    <div className="relative w-full aspect-w-9 aspect-h-16 overflow-hidden rounded-xl">
+                        <div className="video-container">
+                            <video className="object-cover object-center w-full h-full rounded-xl" autoPlay loop muted controls={false}>
+                                <source src="Assets/Videos/Hero_video.mp4" type="video/mp4" />
+                                Your browser does not support the video tag.
+                            </video>
+                        </div>
+                    </div>
+                    <div className="absolute inset-0 flex flex-col items-left justify-center">
+                        <div className="text-white p-4 text-left w-auto">
+                            <div className="text-xl font-bold font-['Plus Jakarta Sans']">
+                                CONCEPTO-23
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
     );
 }
 
