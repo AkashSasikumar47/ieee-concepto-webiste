796cac9ecb7c7d38333b8d7e32e096dff2b629dc
AkashSasikumar47 <akashsasikumar47@gmail.com>
2023-10-02 01:43:03 +0400

    Update AboutConcepto.tsx

--- components/AboutConcepto/AboutConcepto.tsx
+++ components/AboutConcepto/AboutConcepto.tsx
@@ -1,80 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const AboutConcepto = () => {
-    const textStyle = {
-        color: 'var(--text-black)', // Use your specific text color
-        fontSize: '1.125rem', // Adjust font size as needed
-        fontWeight: 'bold',
-        fontFamily: 'Plus Jakarta Sans', // Specify your desired font family
-        lineHeight: '33px', // Adjust line height as needed
-    };
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        setIsMobile(window.innerWidth <= 768);
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 768);
+        };
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
 
     return (
-        <section className="bg-white mb-16">
-            <div className="w-[1920px] h-[850px] px-[84px] py-3 flex-col justify-between items-center inline-flex">
-                <div className="w-[1680px] h-[826px] relative">
-                    <div className="w-[1680px] h-[533px] left-0 top-0 absolute">
-                        <img
-                            className="w-[1680px] h-[533px] left-0 top-0 absolute rounded-[40px]"
-                            src="https://via.placeholder.com/1680x533"
-                        />
-                        <div className="w-[1220px] left-[230px] top-[80px] absolute text-center text-white text-[64px] font-extrabold font-['Plus Jakarta Sans']">
-                            Welcome to CONCEPTO23, an annual event by IEEE SB SRM. We're your gateway to innovation and ideation.
-                        </div>
-                    </div>
-                    <div className="w-[450px] h-[430px] px-4 py-6 left-[112px] top-[396px] absolute bg-white rounded-3xl shadow flex-col justify-start items-center gap-8 inline-flex">
-                        <div className="w-[348px]" style={textStyle}>
-                            Introduction & Purpose
-                        </div>
-                        <div className="w-[348px]" style={textStyle}>
-                            <span>Introduction:</span>
-                            <span>
-                                Welcome to CONCEPTO23, where innovation thrives! We're the annual event by IEEE SB SRM dedicated to nurturing innovation and ideation.
-                                <br />
-                            </span>
-                            <span>Purpose:</span>
-                            <span>
-                                CONCEPTO23 ignites innovation. Join us to explore new ideas, learn from experts, and unleash your potential.
-                            </span>
-                        </div>
-                    </div>
-                    <div className="w-[450px] h-[430px] px-4 py-6 left-[112px] top-[396px] absolute bg-white rounded-3xl shadow flex-col justify-start items-center gap-8 inline-flex">
-                        <div className="w-[348px]" style={textStyle}>
-                            Involvement & Themes
-                        </div>
-                        <div className="w-[348px]" style={textStyle}>
-                            <span>Involvement:</span>
-                            <span>
-                                Industry leaders inspire at CONCEPTO23. Our speakers share insights, sparking your journey.
-                                <br />
-                            </span>
-                            <span>Themes:</span>
-                            <span>
-                                Dive into real-world challenges with our exciting event themes.
-                            </span>
-                        </div>
-                    </div>
-
-                    <div className="w-[450px] h-[430px] px-4 py-6 left-[626px] top-[396px] absolute bg-white rounded-3xl shadow flex-col justify-start items-center gap-8 inline-flex">
-                        <div className="w-[348px]" style={textStyle}>
-                            Benefits & Skills
-                        </div>
-                        <div className="w-[348px]" style={textStyle}>
-                            <span>Benefits:</span>
-                            <span>
-                                CONCEPTO23 offers networking, skill enhancement, and access to valuable resources.
-                                <br />
-                            </span>
-                            <span>Skills:</span>
-                            <span>
-                                Develop teamwork, time management, and communication skills alongside technical know-how.
-                            </span>
+        <div className="relative bg-white mx-auto max-w-[1440px] px-4 md:px-6 lg:px-8 mb-16">
+            {/* Desktop Section */}
+            {!isMobile && (
+                <div className="hidden md:block relative rounded-xl overflow-hidden">
+                    <video className="object-cover object-center w-full h-[512px] rounded-3xl" autoPlay loop muted controls={false}>
+                        <source src="Assets/Videos/Concepto_Video.mp4" type="video/mp4" />
+                        Your browser does not support the video tag.
+                    </video>
+                    <div className="absolute inset-0 flex flex-col items-left justify-center">
+                        <div className="text-white p-8 text-left max-w-[500px]">
+                            <div className="text-3xl font-bold font-['Plus Jakarta Sans']">
+                                Welcome to CONCEPTO23, an annual event by IEEE SB SRM. We're your gateway to innovation and ideation.
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            )}
+
+            {/* Mobile Section */}
+            {isMobile && (
+                <div className="md:hidden relative">
+                    <div className="relative w-full aspect-w-9 aspect-h-16 overflow-hidden rounded-xl">
+                        <div className="video-container">
+                            <video className="object-cover object-center w-full h-full rounded-xl" autoPlay loop muted controls={false}>
+                                <source src="Assets/Videos/Concepto_Video.mp4" type="video/mp4" />
+                                Your browser does not support the video tag.
+                            </video>
+                        </div>
+                    </div>
+                    <div className="absolute inset-0 flex flex-col items-left justify-center">
+                        <div className="text-white p-4 text-left max-w-[200px]">
+                            <div className="text-xs font-medium font-['Plus Jakarta Sans']">
+                                Welcome to CONCEPTO23, an annual event by IEEE SB SRM. We're your gateway to innovation and ideation.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
     );
-};
+}
 
 export default AboutConcepto;
