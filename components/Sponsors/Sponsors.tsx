05f93b55d7e4ff0bb5ad18d28a073f06f5ab4230
AkashSasikumar47 <akashsasikumar47@gmail.com>
2023-10-02 02:40:46 +0400

    Update Sponsors.tsx

--- components/Sponsors/Sponsors.tsx
+++ components/Sponsors/Sponsors.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import './Sponsors.css';
 
 const Sponsors = () => {
     return (
-        <section className="bg-white text-black mb-16">
-            <div className="w-full md:w-[1920px] h-auto p-4 md:p-3 flex flex-col md:flex-row justify-between items-center gap-8">
-                <img className="w-full md:w-[305px] h-auto opacity-30" src="/Assets/Sponsors/Sponsor_1.png" alt="Sponsor Logo 1" />
-                <img className="w-full md:w-[114px] h-auto opacity-30 mt-4 md:mt-0" src="/Assets/Sponsors/Sponsor_2.png" alt="Sponsor Logo 2" />
-                <img className="w-full md:w-[265px] h-auto opacity-30 mt-4 md:mt-0" src="/Assets/Sponsors/Sponsor_3.png" alt="Sponsor Logo 3" />
-                <img className="w-full md:w-80 h-auto opacity-30 mt-4 md:mt-0" src="/Assets/Sponsors/Sponsor_4.png" alt="Sponsor Logo 4" />
+        <section className="bg-white text-black mb-16 overflow-hidden">
+            <div className="sponsors-container">
+                <div className="sponsor">
+                    <img className="sponsor-logo" src="/Assets/Sponsors/Sponsor_1.png" alt="Sponsor Logo 1" />
+                </div>
+                <div className="sponsor">
+                    <img className="sponsor-logo" src="/Assets/Sponsors/Sponsor_2.png" alt="Sponsor Logo 2" />
+                </div>
+                <div className="sponsor">
+                    <img className="sponsor-logo" src="/Assets/Sponsors/Sponsor_3.png" alt="Sponsor Logo 3" />
+                </div>
+                <div className="sponsor">
+                    <img className="sponsor-logo" src="/Assets/Sponsors/Sponsor_4.png" alt="Sponsor Logo 4" />
+                </div>
             </div>
         </section>
     );
