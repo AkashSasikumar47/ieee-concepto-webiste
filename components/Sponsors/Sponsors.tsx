c162d70ea5603c956a9b6385f8055bf30c0294c8
AkashSasikumar47 <akashsasikumar47@gmail.com>
2023-10-05 23:12:14 +0530

    Update code

--- components/Sponsors/Sponsors.tsx
+++ components/Sponsors/Sponsors.tsx
@@ -6,16 +6,19 @@ const Sponsors = () => {
         <section className="bg-white text-black mb-16 overflow-hidden">
             <div className="sponsors-container">
                 <div className="sponsor">
-                    <img className="sponsor-logo" src="/Assets/Sponsors/Sponsor_1.png" alt="Sponsor Logo 1" />
+                    <img className="sponsor-logo" src="/Assets/IEEE-logos/COMSOC.svg" alt="COMSOC" />
                 </div>
                 <div className="sponsor">
-                    <img className="sponsor-logo" src="/Assets/Sponsors/Sponsor_2.png" alt="Sponsor Logo 2" />
+                    <img className="sponsor-logo" src="/Assets/IEEE-logos/CTS.svg" alt="CTS" />
                 </div>
                 <div className="sponsor">
-                    <img className="sponsor-logo" src="/Assets/Sponsors/Sponsor_3.png" alt="Sponsor Logo 3" />
+                    <img className="sponsor-logo" src="/Assets/IEEE-logos/IAS.svg" alt="IAS" />
                 </div>
                 <div className="sponsor">
-                    <img className="sponsor-logo" src="/Assets/Sponsors/Sponsor_4.png" alt="Sponsor Logo 4" />
+                    <img className="sponsor-logo" src="/Assets/IEEE-logos/PELS.svg" alt="PELS" />
+                </div>
+                <div className="sponsor">
+                    <img className="sponsor-logo" src="/Assets/IEEE-logos/RAS.svg" alt="RAS" />
                 </div>
             </div>
         </section>
