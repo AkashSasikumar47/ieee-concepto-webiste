fdf9da7ccb4f9641de4ef643d193f08209c3419b
AkashSasikumar47 <akashsasikumar47@gmail.com>
2023-10-06 22:19:31 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -20,9 +20,16 @@ const Home = () => {
   return (
     <div>
       <Head>
-        <title>CONCEPTO23</title>
-        <meta name="description" content="Your website description" />
+        <title>CONCEPTO-23 | IEEE SRMIST Student Branch</title>
+        <meta name="description" content="CONCEPTO-23 is an annual event organized by IEEE SB SRM, your gateway to innovation, ideation, and a world of opportunities." />
+        <meta name="keywords" content="CONCEPTO-23, IEEE SRMIST, innovation, ideation, opportunities" />
         <link rel="icon" href="/favicon.ico" />
+
+        <meta name="author" content="Akash Sasikumar" />
+        <meta property="og:title" content="CONCEPTO-23 | IEEE SRMIST Student Branch" />
+        <meta property="og:description" content="CONCEPTO-23 is an annual event organized by IEEE SB SRM, your gateway to innovation, ideation, and a world of opportunities." />
+        <meta property="og:image" content="/Assets/Miscellaneous/ogImage.png" />
+        <meta property="og:url" content="https://www.ieeesrmist.com" />
       </Head>
 
       <Navbar />
@@ -36,7 +43,6 @@ const Home = () => {
       <FAQ />
       <Testimonials />
       <Footer />
-
     </div>
   );
 }
