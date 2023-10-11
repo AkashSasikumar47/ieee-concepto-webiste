69296dc96fe87a9910986a05f3fdf1bc8a3bcbd8
AkashSasikumar47 <akashsasikumar47@gmail.com>
2023-10-11 15:30:50 +0530

    Update Footer.tsx

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className="bg-white text-black mt-16">
             <div className="w-full w-[1920px] px-4 px-84 py-3 flex flex-col justify-start items-center gap-6">
                 <a href="/">
-                    <img className="w-[245.19px] h-[100px]" src="/Assets/Logo/IEEE_Logo.png" alt="Footer Logo" />
+                    <img className="w-[720px] h-[100px]" src="/Assets/Logo/Footer_logo.svg" alt="Footer Logo" />
                 </a>
                 <div className="self-stretch px-6 md:px-32 justify-center items-center gap-8 inline-flex flex-col md:flex-row">
                     <a href={`mailto:ieee@srmist.edu.in`}>
@@ -42,7 +42,7 @@ const Footer = () => {
                     <a href="/Contact"> {/* Link to the Contact page */}
                         <div className="text-center text-black text-base font-semibold font-['Plus Jakarta Sans']">Contact</div>
                     </a>
-                    <a href="https://uf0467nch79.typeform.com/to/PPe8o3gV" target="_blank" rel="noopener noreferrer">
+                    <a href="https://forms.gle/yeQDynfTv4P8eFWx9" target="_blank" rel="noopener noreferrer">
                         {/* Register button with a link to the registration form */}
                         <div className="text-center text-black text-base font-semibold font-['Plus Jakarta Sans']">Register</div>
                     </a>
