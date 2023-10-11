201e09e00e5d54f4dd59628f012ff544cf20502e
AkashSasikumar47 <akashsasikumar47@gmail.com>
2023-10-11 15:30:43 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -24,6 +24,7 @@ const Home = () => {
         <meta name="description" content="CONCEPTO-23 is an annual event organized by IEEE SB SRM, your gateway to innovation, ideation, and a world of opportunities." />
         <meta name="keywords" content="CONCEPTO-23, IEEE SRMIST, innovation, ideation, opportunities" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.ico" />
 
         <meta name="author" content="Akash Sasikumar" />
         <meta property="og:title" content="CONCEPTO-23 | IEEE SRMIST Student Branch" />
@@ -31,6 +32,7 @@ const Home = () => {
         <meta property="og:image" content="/Assets/Miscellaneous/ogImage.png" />
         <meta property="og:url" content="https://www.ieeesrmist.com" />
       </Head>
+
 
       <Navbar />
       <Hero />
