ee652f3e951e370e8ce8a5a972660527d59747d0
AkashSasikumar47 <akashsasikumar47@gmail.com>
2024-04-19 23:31:07 +0530

    Updated code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,60 +1,25 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "CONCEPTO-24 | IEEE SRMIST",
+  description: "CONCEPTO-24 is an annual event organized by IEEE SB SRM, your gateway to innovation, ideation, and a world of opportunities.",
+};
 
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode
-}) {
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en">
       <head>
-        <meta charSet="UTF-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-        <title>CONCEPTO-23 | IEEE SRMIST Student Branch</title>
-        <meta name="description" content="Join us at CONCEPTO-23, an annual event by IEEE SRMIST, your gateway to innovation and learning." />
-        <meta name="keywords" content="CONCEPTO, IEEE SRMIST, innovation, learning, annual event" />
-
-        <meta name="author" content="Akash Sasikumar" />
-
-        <meta property="og:title" content="CONCEPTO-23 | IEEE SRMIST Student Branch" />
-        <meta property="og:description" content="Join us at CONCEPTO-23, an annual event by IEEE SRMIST, your gateway to innovation and learning." />
-        <meta property="og:image" content="/Assets/Miscellaneous/ogImage.png" />
-        <meta property="og:url" content="https://www.ieeesrmist.com" />
-
-        <script type="application/ld+json">
-          {`
-            {
-              "@context": "http://schema.org",
-              "@type": "Event",
-              "name": "CONCEPTO-23",
-              "startDate": "2023-10-18T09:00",
-              "endDate": "2023-10-19T17:00",
-              "description": "Join us at CONCEPTO-23, an annual event by IEEE SRMIST, your gateway to innovation and learning.",
-              "location": {
-                "@type": "Place",
-                "name": "SRM Institute of Science and Technology Kattankulathur, Tech Park, TP-501/502",
-                "address": {
-                  "@type": "PostalAddress",
-                  "streetAddress": "Kattankulathur, Tech Park, TP-501/502",
-                  "addressLocality": "Chennai",
-                  "addressRegion": "TN",
-                  "postalCode": "603203",
-                  "addressCountry": "India"
-                }
-              },
-              "image": "/Assets/Miscellaneous/ogImage.png",
-              "url": "https://www.ieeesrmist.com"
-            }
-          `}
-        </script>
-
+        <link rel="icon" href="/favicon.ico" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -14,23 +14,16 @@ import FAQ from '../components/FAQ/FAQ';
 import Testimonials from '../components/Testimonial/Testimonial';
 import Footer from '../components/Footer/Footer';
 
-import '../app/globals.css';
+import './globals.css';
 
 const Home = () => {
   return (
-    <div>
+    <div className="bg-stone-50">
       <Head>
-        <title>CONCEPTO-23 | IEEE SRMIST Student Branch</title>
-        <meta name="description" content="CONCEPTO-23 is an annual event organized by IEEE SB SRM, your gateway to innovation, ideation, and a world of opportunities." />
-        <meta name="keywords" content="CONCEPTO-23, IEEE SRMIST, innovation, ideation, opportunities" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.ico" />
-
-        <meta name="author" content="Akash Sasikumar" />
-        <meta property="og:title" content="CONCEPTO-23 | IEEE SRMIST Student Branch" />
-        <meta property="og:description" content="CONCEPTO-23 is an annual event organized by IEEE SB SRM, your gateway to innovation, ideation, and a world of opportunities." />
-        <meta property="og:image" content="/Assets/Miscellaneous/ogImage.png" />
-        <meta property="og:url" content="https://www.ieeesrmist.com" />
+        <title>CONCEPTO-24 | IEEE SRMIST</title>
+        <meta name="description" content="CONCEPTO-24 is an annual event organized by IEEE SB SRM, your gateway to innovation, ideation, and a world of opportunities." />
+        <meta name="keywords" content="PROTOCOL 1.0, hackathon, IEEE SRMIST, computer science, engineering, innovation, technology" />
+        <link rel="icon" href="/favicon.ico?v=2" />
       </Head>
 
       <Navbar />
